baa4333ed655350be752fc25e056fdeed21d16d9
anja22 <anja.syring@sh-family.de>
2018-01-24 09:46:11 +0100

    Initial set up node extractor

---
+++ src/main/java/org/rapidprom/operators/extract/ExtractNodesOperator.java
@@ -0,0 +1,32 @@
+package org.rapidprom.operators.extract;
+
+import com.rapidminer.operator.IOObject;
+import com.rapidminer.operator.Operator;
+import com.rapidminer.operator.OperatorDescription;
+import com.rapidminer.operator.OperatorException;
+import com.rapidminer.operator.learner.PredictionModel;
+import com.rapidminer.operator.learner.tree.Tree;
+import com.rapidminer.operator.ports.InputPort;
+import weka.classifiers.trees.J48;
+import weka8.classifiers.Classifier;
+
+public class ExtractNodesOperator extends Operator{
+	
+	/** defining the ports */
+	private InputPort modelInput = getInputPorts().createPort("Tree model");
+
+	public ExtractNodesOperator(OperatorDescription description) {
+		super(description);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void doWork() throws OperatorException {
+		
+		IOObject object = modelInput.getAnyDataOrNull();
+//		PredictionModel model = (PredictionModel) object;
+		Classifier tree =  (Classifier) object;
+//		J48 wekaTree = (J48) object;
+	}
+	
+}

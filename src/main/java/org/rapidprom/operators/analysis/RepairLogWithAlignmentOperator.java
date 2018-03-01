110fa17f96714d0014920401b3871250ab53941f
anja22 <anja.syring@sh-family.de>
2018-03-01 17:44:08 +0100

    Adjusted operator to work with either Decision tree rules or frequent item sets

--- src/main/java/org/rapidprom/operators/analysis/RepairLogWithAlignmentOperator.java
+++ src/main/java/org/rapidprom/operators/analysis/RepairLogWithAlignmentOperator.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.deckfour.xes.factory.XFactoryRegistry;
 import org.deckfour.xes.model.XLog;
 import org.processmining.datapetrinets.DataPetriNet;
 import org.processmining.datapetrinets.expression.GuardExpression;
@@ -28,16 +29,25 @@ import org.rapidprom.operators.util.RapidProMProgress;
 import com.rapidminer.operator.Operator;
 import com.rapidminer.operator.OperatorDescription;
 import com.rapidminer.operator.OperatorException;
+import com.rapidminer.operator.learner.associations.FrequentItemSet;
+import com.rapidminer.operator.learner.associations.FrequentItemSets;
 import com.rapidminer.operator.ports.InputPort;
 import com.rapidminer.operator.ports.OutputPort;
 import com.rapidminer.operator.ports.metadata.GenerateNewMDRule;
+import com.rapidminer.parameter.ParameterType;
+import com.rapidminer.parameter.ParameterTypeCategory;
 import com.rapidminer.tools.LogService;
 
 public class RepairLogWithAlignmentOperator extends Operator {
 	
+	private static final String RULESOURCE = "Source";
+	private static final String DECISIONTREE = "Decision Tree rules";
+	private static final String FREQUENTITEMSET = "Frequent Itemset";
+	
 	private InputPort alignmentInput = getInputPorts().createPort("alignments (ProM ResultReplay)", ResultReplayIOObject.class);
 	private InputPort modelInput = getInputPorts().createPort("model (DataPetriNet)", PetriNetIOObject.class);
-	private InputPort extractInput = getInputPorts().createPort("guard expression (WekaTree)", GuardExpressionIOObject.class);
+//	private InputPort extractInput = getInputPorts().createPort("guard expression (WekaTree)", GuardExpressionIOObject.class);
+	private InputPort extractInput = getInputPorts().createPort("repair rules");
 	private OutputPort logOutput = getOutputPorts().createPort("event log (ProM Event Log)");
 	
 	
@@ -60,12 +70,18 @@ public class RepairLogWithAlignmentOperator extends Operator {
 		
 		DataPetriNetIOObject dpnIOObject = modelInput.getData(DataPetriNetIOObject.class);
 		DataPetriNet net = dpnIOObject.getArtifact();
+		AlignmentBasedLogRepairParametersImpl config;
 		
-		GuardExpressionIOObject expressionIOObject = extractInput.getData(GuardExpressionIOObject.class);
-		GuardExpression expression = expressionIOObject.getArtifact();
+		if(getParameterAsString(RULESOURCE).equals(DECISIONTREE)) {
+			GuardExpressionIOObject expressionIOObject = extractInput.getData(GuardExpressionIOObject.class);
+			GuardExpression expression = expressionIOObject.getArtifact();
+			config = getConfiguration(alignments,expression);
+		}else {
+			FrequentItemSets fiSet = extractInput.getData(FrequentItemSets.class);
+			config = getConfiguration(alignments,fiSet);
+		}
 		
-		AlignmentBasedLogRepairParametersImpl config = getConfiguration(alignments,expression);
-		
+				
 		XLog log = RepairLog.plugin(context, alignments, net, config);
 		logOutput.deliver(new XLogIOObject(log, context));
 
@@ -73,17 +89,8 @@ public class RepairLogWithAlignmentOperator extends Operator {
 				"End: repair log with respect to alignment (" + (System.currentTimeMillis() - time) / 1000 + " sec)");
 	}
 
-	
-	private AlignmentBasedLogRepairParametersImpl getConfiguration(ResultReplay alignments,GuardExpression expression) {
+	private AlignmentBasedLogRepairParametersImpl getConfiguration(ResultReplay alignments, List<String> valueAsList) {
 		
-		String guard = expression.toString();
-		String[] value = guard.replace("(", "").replace(")", "").replace("==","").replace("!=","").replaceAll("\"[0-9]\"","").replaceAll("\"[0-9].[0-9]\"","").trim().split("&&");
-		List<String> valueAsList = Arrays.asList(value);
-		
-		for (String values : valueAsList) {
-			System.out.println(values);
-		}
-				
 		AlignmentBasedLogRepairParametersImpl params = new AlignmentBasedLogRepairParametersImpl();
 		String label = null;
 		String alignLabel = null;
@@ -101,7 +108,7 @@ public class RepairLogWithAlignmentOperator extends Operator {
 							String ml = "Move_log_";
 							alignLabel = ml.concat(label.replaceAll(" ", "_"));
 //							System.out.println("Break move log " + alignLabel);
-							if (valueAsList.contains(alignLabel)){
+							if (!valueAsList.isEmpty() && valueAsList.contains(alignLabel)){
 //								System.out.println("Break move log " + label);
 								break;
 							}else {
@@ -112,19 +119,19 @@ public class RepairLogWithAlignmentOperator extends Operator {
 							label = align.getProcessView().getActivity();
 							String sm = "Sync_move_";
 							alignLabel = sm.concat(label.replaceAll(" ", "_"));
-//							if (valueAsList.contains(alignLabel)){
-								//System.out.println("Break no good move " + label);
-//								break;
-//							}else {
+							if (!valueAsList.isEmpty() && valueAsList.contains(alignLabel)){
+								System.out.println("Break no good move " + label);
+								break;
+							}else{
 								params.getSyncMoves().add(label);
 								break;
-//							}
+							}
 						case MREAL :
 							label = align.getProcessView().getActivity();
 							String mm = "Move_model_";
 							alignLabel = mm.concat(label.replaceAll(" ", "_"));
 //							System.out.println("Break move model " + alignLabel);
-							if (valueAsList.contains(alignLabel)) {	
+							if (!valueAsList.isEmpty() && valueAsList.contains(alignLabel)) {	
 //								System.out.println("Break move model " + label);
 								break;
 							}else {
@@ -137,6 +144,64 @@ public class RepairLogWithAlignmentOperator extends Operator {
 				}
 			}
 		
+		return params;
+	}
+	
+	private AlignmentBasedLogRepairParametersImpl getConfiguration(ResultReplay alignments,GuardExpression expression) {
+		
+		String guard = expression.toString();
+		String[] value = guard.replace("(", "").replace(")", "").replace("==","").replace("!=","").replaceAll("\"[0-9]\"","").replaceAll("\"[0-9].[0-9]\"","").trim().split("&&");
+		List<String> valueAsList = Arrays.asList(value);
+		
+//		for (String values : valueAsList) {
+//			System.out.println(values);
+//		}
+				
+		return getConfiguration(alignments,valueAsList);
+	}
+	
+	private AlignmentBasedLogRepairParametersImpl getConfiguration(ResultReplay alignments,FrequentItemSets frequentItemSets) {
+		
+		List<String> valueAsList = new LinkedList<String>();
+		
+		if (frequentItemSets.size() > 0) {
+			
+			int maxSize = frequentItemSets.getMaximumSetSize();
+			
+			for (FrequentItemSet itemSet : frequentItemSets) {
+				
+				//TODO get biggest frequentset
+				if(itemSet.getNumberOfItems()==maxSize) {
+					
+					String[] value = itemSet.getItemsAsString().split(", ");
+					valueAsList = Arrays.asList(value);
+					
+					for (String values : valueAsList) {
+						System.out.println(values);
+					}
+					
+//					System.out.println("Size: "+ itemSet.getNumberOfItems() + " Items: " + itemSet.getItemsAsString());
+//					valueAsList.add(itemSet.getItemsAsString());
+				}
+				
+			}
+			
+		}
+		
+//		String guard = expression.toString();
+//		String[] value = guard.replace("(", "").replace(")", "").replace("==","").replace("!=","").replaceAll("\"[0-9]\"","").replaceAll("\"[0-9].[0-9]\"","").trim().split("&&");
+		
+
+				
+		return getConfiguration(alignments,valueAsList);
+	}
+	
+	public List<ParameterType> getParameterTypes() {
+		List<ParameterType> params = super.getParameterTypes();
+
+		String[] options = {DECISIONTREE,FREQUENTITEMSET};
+		
+		params.add(new ParameterTypeCategory(RULESOURCE , "Source that defines which steps to repair.",	options , 1, false));
 		return params;
 	}
 }
